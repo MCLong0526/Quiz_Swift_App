136c19c963a103e1aa6a984a6c51a760e013aca8
Michael526-dev <michael.chia@bunkerchain.io>
2025-06-18 07:31:38 +0800

    update some question and logic

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,10 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var progressLabel: UIProgressView!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    var counter = 0
+    var wrongButton: UIButton?
+    let quiz = [
+        ["Four + Two is equal to Six", "True"],
+        ["Fix - Three is greater than One","True"],
+        ["Three + Eight is less than Ten","False"]]
+                
+    
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        if(sender.titleLabel?.text == quiz[counter][1]){
+            if(counter<quiz.count-1){
+                counter+=1
+            }else{
+                counter = 0;
+            }
+        }else{
+            wrongButton = sender
+            markWrongAnswer()
+        }
+        updateUI()
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
     }
+    
+    func updateUI (){
+        questionLabel.text = quiz[counter][0]
+    }
+    
+    
+    @objc func markWrongAnswer() {
+        wrongButton?.setTitleColor(.red, for: .normal)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                self.wrongButton?.setTitleColor(.white, for: .normal)
+            }
+    }
+    
 
 
 }
